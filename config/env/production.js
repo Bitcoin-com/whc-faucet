bbed044ca013764311a8a73b9fb288023aebe0cf
Chris Troutner <chris.troutner@gmail.com>
2018-10-25 13:06:38 -0700

    Chaning mongodb port

--- config/env/production.js
+++ config/env/production.js
@@ -3,6 +3,6 @@
 module.exports = {
   session: "secret-boilerplate-token",
   token: "secret-jwt-token",
-  database: "mongodb://172.17.0.1:3500/whc-faucet-server-prod",
+  database: "mongodb://172.17.0.1:3501/whc-faucet-server-prod",
   port: 3456
 }
